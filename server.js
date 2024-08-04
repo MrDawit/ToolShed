9d93d52cbac008b709384b9db547ead84c546241
Shalom Dawit <Shalom.Dawit@gmail.com>
2024-08-04 14:10:06 -0400

    added webpage direct open through child_process

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require('path');
 const app = express();
-
+//Whenever changing ports/site, Okta profile on Okta website needs to be updated 
+//along with CORS access to port/site on different section of Okta website
+const PORT = process.env.PORT || 3001;
 //ADD DB MODEL
 const db = require("./models");
 
@@ -54,13 +56,24 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/', function(req, res) {
     res.sendFile('client/build', 'index.html');
   });
-  console.log("server in production mode");
-}
 
-
-
-// set port, listen for requests
-const PORT = process.env.PORT || 3001;
+  //listen for requests
+  app.listen(PORT, () => {
+    if (process.env.NODE_ENV_PRODUCTION_TESTING === 'true') {
+        //opens up localhost address on browser (windows)
+        require('child_process').exec(`start http://localhost:${PORT}/`);
+        console.log(`Server is running on port ${PORT} in production mode.`);
+    }else {
+          //opens up production address on browser (windows)
+          require('child_process').exec(`start https://toolshed.onrender.com/`);
+          console.log(`Server is running on port ${PORT} in production address.`);
+    }    
+  });
+  
+  //console.log("server in production mode");
+}else{
+//listen for requests in development mode
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT} in development mode.`);
 });
+}
